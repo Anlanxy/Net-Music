fbdc933aac10070ba91213c26b1a061d7c95bfb4
黯蓝心忧 <61707124+Anlanxy@users.noreply.github.com>
2020-06-29 16:47:00 +0800

    Update github.js

--- github.js
+++ github.js
@@ -17,7 +17,7 @@
  * 📌 如果希望监控Release，请切换至Release界面，直接复制URL填入；
  */
 
-let token = "a6b95029be66cff0b64dd6a9821d18367e05d9e2";
+let token = "784a03feb07989d3339dfa41c7eb41777436cbfa";
 
 let repositories = [
     {
